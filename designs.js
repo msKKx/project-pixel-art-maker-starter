534b1d2d1b96b973f9b845f81084856203115803
Kim Roesner <kimroesner@kimroesner.speedport_w_724v_typ_a_05011603_06_003>
2020-05-03 22:08:19 +0200

    Pixel-Art-Maker Project Udacity resolved

--- designs.js
+++ designs.js
@@ -1,10 +1,28 @@
-// Select color input
-// Select size input
+let size = document.getElementById('sizePicker'); //select size 
+let grid = document.getElementById('pixelCanvas'); // select pixel canvas
+let height = 0; // select height 
+let width = 0; // select width 
 
-// When size is submitted by the user, call makeGrid()
-
-function makeGrid() {
-
-// Your code goes here!
-
+function makeGrid(height, width) { // function, that creates a canvas of the size set by the user
+    for (i = 0; i < height; i++) {
+        var row = grid.insertRow(i); // row of the table
+        for (j = 0; j < width; j++) {
+            var cell = row.insertCell(j); // cell of the table 
+            cell.addEventListener('click', function (event) { // event, that colors the pixel in the grid, if the user clicks on it
+                let color = document.getElementById('colorPicker').value; // select color
+                event.target.style.backgroundColor = color; // change background color 
+            })
+        }
+    }
 }
+
+size.addEventListener('submit', function (event) {//event, that calls the makeGrid function, if the submit button is pressed
+    for (i=0; i< height; i++){
+        grid.deleteRow(i);
+    }
+    height = document.getElementById('inputHeight').value; // select height 
+    width = document.getElementById('inputWidth').value; // select width 
+    event.preventDefault();
+    makeGrid(height, width);
+})
+
